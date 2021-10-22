f4e05980cab571c23c2050270c732066a049653c
fatemehnejati <92602732+fatemehnejati@users.noreply.github.com>
2021-10-22 11:48:19 +0330

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <stdlib.h>  
+using namespace std; 
+ 
+int main()
+{
+	int n, i;
+	cin >> n;
+	for(i= 1; i <= n; i++)
+	{
+		int f= i % 2;
+		if(f != 0) cout <<"*";
+		if(f == 0)  cout <<"#";
+	}
+	return 0; 
+}
+
